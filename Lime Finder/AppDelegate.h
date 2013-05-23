87228e6868809222d17857c0de760d17cfe1115b
idstorm <idstorm@hyeon-yong-ui-MacBook.local>
2013-05-23 20:47:04 +0900

    Initial Commit

---
+++ Lime Finder/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Lime Finder
+//
+//  Created by idstorm on 13. 5. 23..
+//  Copyright (c) 2013년 Lime Works. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Lime Finder/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Lime Finder
+//
+//  Created by idstorm on 13. 5. 23..
+//  Copyright (c) 2013년 Lime Works. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
